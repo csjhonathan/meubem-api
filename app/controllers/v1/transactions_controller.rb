47ac43e0b0d865cbbf4f3bba7e7ca55eabb2f357
csjhonathan <jhonathancarv.s@gmail.com>
2024-04-23 11:21:20 -0300

    implementa crud completo de transações

--- app/controllers/v1/transactions_controller.rb
+++ app/controllers/v1/transactions_controller.rb
@@ -1,12 +1,13 @@
 class V1::TransactionsController < ApplicationController
+  before_action :set_transaction, only: [:show, :update, :destroy]
+
   def index
     transactions = Transaction.where(account_id: @current_user.account.id)
     render json: { data: transactions}, status: :ok
   end
 
   def show 
-    transaction = Transaction.find_by(id: params[:id])
-    render json: { data: transaction }, status: :ok
+    render json: { data: @transaction }, status: :ok
   end
 
   def create
@@ -17,7 +18,29 @@ class V1::TransactionsController < ApplicationController
       render json: { data: transaction.errors }, status: :unprocessable_entity
     end
   end
+  
+  def update
+    if @transaction.update(transaction_params)
+      render json: { data: @transaction }, status: :ok
+    else
+      render json: { data: @transaction.errors }, status: :unprocessable_entity
+    end
+  end
 
+  def destroy
+    if @transaction.discard
+      render json: { data: "Transação deletada com sucesso!" }, status: :ok
+    else
+      render json: { data: @transaction.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_transaction
+    @transaction = Transaction.find_by(id: params[:id])
+    render json: { data: "Transação não encontrada ou deletada!" }, status: :not_found unless @transaction.present?
+  end
 
   def transaction_params
     params.require(:transaction).permit(
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -12,14 +12,40 @@ class Transaction < ApplicationRecord
   validates :value, presence: true, numericality: { greater_than: 0 }
   validates :kind, presence: true
 
-  after_save :update_account_balance
+  before_save :update_account_balance
+  before_update :remove_value_from_account_balance
 
   def update_account_balance
     account = Account.find(account_id)
-    if self.kind == "income"
-      account.balance += self.value
-    else
-      account.balance -= self.value
+    
+    if self.kind_was.present? && kind_changed? 
+      if self.kind == "income"
+        account.balance += self.value_was * 2
+      else
+        account.balance -= self.value_was * 2
+      end
+    end
+
+    if value_changed?
+      prevValue = self.value_was || 0
+      if self.kind == "income"
+        account.balance += (self.value - prevValue)
+      else
+        account.balance -= (self.value - prevValue)
+      end
+    end
+    account.save
+  end
+
+  def remove_value_from_account_balance
+    account = Account.find(account_id)
+
+    if self.discarded_at.present?
+      if self.kind == "income"
+        account.balance -= self.value
+      else
+        account.balance += self.value
+      end
     end
     account.save
   end
